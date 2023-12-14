b0738b3b7b52529d435f682db1d3e3daeb9c0de8
cfgm1603 <cesargimo@unisabana.edu.co>
2023-12-14 13:34:00 -0500

    Mejorado el n avbar adaptativo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,56 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import img from "../images/logo.png";
 
 const Navbar = () => {
-    return (
-        <nav>
-        <h2>Logo</h2>
-            <ul>
-                <li>
-                    <Link to="/soluciones">Soluciones</Link>
-                </li>
-                <li>
-                    <Link to="/">Contacto</Link>
-                </li>
-                <li>
-                    <Link to="/">test</Link>
-                </li>
-                <li>
-                    <Link to="/">test</Link>
-                </li>
-            </ul>
-        </nav>
-    );
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav>
+      <div className="header">
+        <Link to="/">
+          {" "}
+          <img src={img} alt="logo" />
+        </Link>
+        <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="40"
+            height="40"
+            fill="currentColor"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M2 12.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"
+            />
+          </svg>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+      <ul className={menuOpen ? "open" : ""}>
+        <li>
+          <Link to="/soluciones">Soluciones</Link>
+        </li>
+        <li>
+          <Link to="/">Colegios</Link>
+        </li>
+        <li>
+          <Link to="/">Compañia</Link>
+        </li>
+        <li>
+          <Link to="/">Login</Link>
+        </li>
+        <li>
+          <Link to="/">Comunicate con ventas</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
-
 export default Navbar;
-
