24f3bc43c94af7e34842d751ea8ca786b290e4b6
cfgm1603 <cesargimo@unisabana.edu.co>
2023-12-19 01:28:10 -0500

    Finalizado Home

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     
     <Router>
-      <div>
+      <div >
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/soluciones" element={<Soluciones />} />
